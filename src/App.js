8b29700fd0974d5bc2445a91107562638640eb16
Zulkarnain Shah <m.zulkarnain.shah@gmail.com>
2017-09-26 22:31:16 +0530

    Converted App into class based Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text, ScrollView } from 'react-native';
 import { Header, InlineInput, Button } from './components/common';
 
-const App = () => {
-  return (
-    <View style={styles.viewStyle}>
-      <Header title='MocoDot' backgroundColor='#44454b' titleColor='#FFFFFF' height={150} />
-      <Text style={styles.textStyle}>SIGN UP</Text>
+class App extends Component {
 
-      <ScrollView style={{ flex: 1 }}>
-        <InlineInput placeholder='Business Name' />
-        <InlineInput placeholder='Contact Name' />
-        <InlineInput placeholder='Email' keyboardType='email-address' />
-        <InlineInput placeholder='Password' secureTextEntry />
-        <InlineInput placeholder='Phone' keyboardType='phone-pad' />
-        <Button titleColor='#5e606b'> CONFIRM </Button>
-      </ScrollView>
+  componentWillMount() {
+    //TODO: Call GET serviceSector API 
+  }
 
-    </View>
-  );
-};
+  render() {
+    return (
+      <View style={styles.viewStyle}>
+        <Header title='MocoDot' backgroundColor='#44454b' titleColor='#FFFFFF' height={150} />
+        <Text style={styles.textStyle}>SIGN UP</Text>
+
+        <ScrollView style={{ flex: 1 }}>
+          <InlineInput placeholder='Business Name' />
+          <InlineInput placeholder='Contact Name' />
+          <InlineInput placeholder='Email' keyboardType='email-address' />
+          <InlineInput placeholder='Password' secureTextEntry />
+          <InlineInput placeholder='Phone' keyboardType='phone-pad' />
+          <Button titleColor='#5e606b'> CONFIRM </Button>
+        </ScrollView>
+
+      </View>
+      );
+    }
+}
 
 const styles = {
   textStyle: {
